af908b77e4f6300ab2e54ee7a373216c64e3edab
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-10 06:19:02 +0100

    test: Perform tests on index

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,19 +1,26 @@
-import { response } from 'express';
 import supertest from 'supertest';
 
 import app from '../index';
 
-
 describe('Server Tests.', () => {
   const request = supertest(app);
 
-  it('Expects GET / to be 200 OK ', async () => {
-    const response = await request.get('/');
-    expect(response.status).toBe(200);
+  describe('Tests for index GET / route', () => {
+    it('Expects GET / to be 200 OK ', async () => {
+      const response = await request.get('/');
+      expect(response.status).toBe(200);
+    });
+
+    it('Expects GET / to have a "Hello!" body', async () => {
+      const response = await request.get('/');
+      expect(response.text).toEqual('Hello!');
+    });
   });
 
-  it('Expects /error-url to be 404', async () => {
-    const response = await request.get('/error-url');
-    expect(response.status).toBe(404);
+  describe('Tests for error endpoints', () => {
+    it('Expects /error-url to be 404', async () => {
+      const response = await request.get('/error-url');
+      expect(response.status).toBe(404);
+    });
   });
 });

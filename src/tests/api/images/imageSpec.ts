b23ae24a964cc6cfb20f5d96532d0b2f5142da47
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-11 22:18:29 +0100

    test: Test image route

---
+++ src/tests/api/images/imageSpec.ts
@@ -0,0 +1,36 @@
+import supertest from 'supertest';
+
+import app from '../../../index';
+
+const request = supertest(app);
+
+describe('Test /api/images route', () => {
+  describe('Test valid requests', () => {
+    const filename = 'fjord.jpg';
+
+    it(`Expects GET /api/images?filename=valid-file.jpg to be 200`, async () => {
+      const response = await request.get(`/api/images?filename=${filename}`);
+      expect(response.status).toBe(200);
+    });
+
+    it(`Expects GET /api/images?filename=${filename} image response`, async () => {
+      const response = await request.get(`/api/images?filename=${filename}`);
+      expect(response.type).toEqual('image/jpeg');
+    });
+  });
+
+  describe('Test invalid requests', () => {
+    it('Expects GET /api/images to be 400', async () => {
+      const response = await request.get('/api/images');
+      expect(response.status).toBe(400);
+    });
+
+    it(`Expects GET /api/images?filename=invalid.jpg to be 404`, async () => {
+      const response = await request.get(
+        '/api/images?filename=invalid.jpg&width=400&height=200'
+      );
+
+      expect(response.status).toBe(404);
+    });
+  });
+});

19297699581de9bd5cd34d71eb906d964bee1f8a
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2022-12-14 04:27:34 +0100

    test: Pass caching test

--- src/tests/api/images/imageSpec.ts
+++ src/tests/api/images/imageSpec.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest';
+import { promises as fs } from 'fs';
 
 import app from '../../../index';
+import { thumbsDir } from '../../../api/utilities/resize';
 
 const request = supertest(app);
 
@@ -31,6 +33,31 @@ describe('Test /api/images route', () => {
       );
 
       expect(response.status).toBe(404);
+    });
+  });
+
+  describe('Tests for Caching', () => {
+    let cacheSize: number;
+    const CACHE_LIMIT = 30;
+    const filename = 'fjord.jpg';
+    
+    beforeAll(async () => {
+      const maxRequest = 40
+      for(let i = 0, width = 100; i <= maxRequest; i++){
+        if (i < maxRequest/2){
+          await request.get(`/api/images?filename=${filename}&width=${width}`);
+          width += 50;
+        }else{
+          await request.get(`/api/images?filename=${filename}&height=${width}`);
+          width -= 50;
+        }
+      }
+      
+      cacheSize = (await fs.readdir(thumbsDir)).length;
+    })
+
+    it('Expects cache size to be less than or equal to CACHE_LIMIT', () => {
+      expect(cacheSize).toBeLessThanOrEqual(CACHE_LIMIT)
     });
   });
 });
--- src/tests/api/utilities/resizeSpec.ts
+++ src/tests/api/utilities/resizeSpec.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import sharp from 'sharp';
 
-import resize from '../../../api/utilities/resize';
+import { resize } from '../../../api/utilities/resize';
 
 describe('Test function resize()', () => {
   const fullImgDir = path.join(__dirname, '../../../../assets/images/full');
